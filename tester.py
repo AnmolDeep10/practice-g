2300a61f1d95882feca56a78370e26bd3c46f5df
Anmoldeep singh <anmoldeep25825@gmail.com>
2025-08-14 00:05:12 +0530

    these are the new changes

--- tester.py
+++ tester.py
@@ -2,3 +2,13 @@ print("hello world")
 
 def main():
     print("This is the main function.")
+    hlo = "Hello, World!"
+    print(hlo)
+
+def add(a, b):
+    result = []
+    for i in range(len(a)):
+        result.append(a[i] + b[i])
+    return result
+
+add([1, 2, 3], [4, 5, 6])
